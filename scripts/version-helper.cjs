36d84f4b17728327b4f02505b60248cc5020e5ac
basuni shibah <basuni@akarprima.com>
2025-06-21 16:44:56 +0700

    chore(release): bump version to 1.0.0

--- scripts/version-helper.cjs
+++ scripts/version-helper.cjs
@@ -3,12 +3,12 @@ const pkg = require('../package.json');
 
 const version = pkg.version;
 
-try {
+try
+{
   execSync(`git commit -am "chore(release): bump version to ${version}"`, { stdio: 'inherit' });
-  execSync(`git tag v${version}`, { stdio: 'inherit' });
   execSync(`git push`, { stdio: 'inherit' });
-  execSync(`git push --tags`, { stdio: 'inherit' });
-} catch (err) {
+} catch (err)
+{
   console.error("❌ Gagal tagging versi:", err.message);
   process.exit(1);
 }
